cfaafb15c154781481f6c99f286696cb7d59c079
Redzselek <egyedirobika@gmail.com>
2024-06-05 13:50:05 +0200

    Masodik commit Nem mukodik a feltoltes

--- GyartoSoros/MainWindow.xaml.cs
+++ GyartoSoros/MainWindow.xaml.cs
@@ -25,5 +25,39 @@ namespace GyartoSoros
             InitializeComponent();
         }
 
+        private void KovetkezoKor_Click(object sender, RoutedEventArgs e)
+        {
+            int meret = 8;
+            if (TablaMeret.SelectedIndex == 0)
+            {
+                meret = 8;
+            }
+            if (TablaMeret.SelectedIndex == 1)
+            {
+                meret = 12;
+            }
+            if (TablaMeret.SelectedIndex == 2)
+            {
+                meret = 16;
+            }
+            for (int i = 0; i < meret; i++)
+            {
+                Rectangle doboz = new Rectangle();
+                doboz.Height = 100;
+                doboz.Width = 100;
+                doboz.Fill = new SolidColorBrush(Colors.Blue);
+                doboz.Margin = new Thickness(10, 10, 10, 10);
+                Grd_2.Children.Add(doboz);
+            }
+            for (int i = 0; i < meret; i++)
+            {
+                Rectangle doboz = new Rectangle();
+                doboz.Height = 100;
+                doboz.Width = 100;
+                doboz.Fill = new SolidColorBrush(Colors.Red);
+                doboz.Margin = new Thickness(10, 10, 10, 10);
+                Grd_1.Children.Add(doboz);
+            }
+        }
     }
 }
